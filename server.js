76a60ff694a18194aa039e2e0e013ab480f8bbbf
raju9491895580 <rajg97136@gmail.com>
2025-02-11 10:27:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,4 +44,5 @@ app.post("/login", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
+
